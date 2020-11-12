a80f9be813315218edf2508f9419fa06b5c867a2
lipi luis <lipiluis0101@gmail.com>
2020-11-11 21:49:45 -0300

    Melhorando a implementação do metodo tabuleiro

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -8,10 +8,25 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos.toPosicao());
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 0));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(6, 4));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(4, 4));
+                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 1));
+
+                Tela.ImprimirTabuleiro(tab);
+
+            }
+            catch(TabuleiroException x)
+            {
+                Console.WriteLine(x);
+            }
             Console.ReadKey();
         }
     }
