32a1186e60993e0b057a2143454c372add280e5b
lipi luis <lipiluis0101@gmail.com>
2020-11-11 21:31:17 -0300

    Tratando erros com exerção personalizadas

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -8,13 +8,22 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Tela.ImprimirTabuleiro(tab);
+                Tela.ImprimirTabuleiro(tab);
+
+            }
+            catch(TabuleiroException x)
+            {
+                Console.WriteLine(x.Message);
+            }
             Console.ReadKey();
         }
     }
